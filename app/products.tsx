ffb1883ca6ccb28163f2bd344f06b702bfde1c95
uzma shaik <uzma97910@gmail.com>
2025-07-01 17:58:15 +0530

    feat: add products data with images and pricing

---
+++ app/products.tsx
@@ -0,0 +1,24 @@
+import image1 from "@/public/images/dummyImage-1.jpg";
+import image2 from "@/public/images/dummyImage-2.jpg";
+import image3 from "@/public/images/dummyImage-3.jpeg";
+
+export const products = [
+  {
+    id: 1,
+    name: "Vintage Berkin",
+    price: 1200,
+    image: image1,
+  },
+  {
+    id: 2,
+    name: "Gucci",
+    price: 1500,
+    image: image2,
+  },
+  {
+    id: 3,
+    name: "Louis Vuitton",
+    price: 2000,
+    image: image3,
+  },
+];
