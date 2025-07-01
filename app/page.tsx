b6896c4ec165ac96c62ccbfed3e611272c1022c8
uzma shaik <uzma97910@gmail.com>
2025-07-01 20:17:22 +0530

    feat: remove Navbar component from Home for layout simplification

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 import Link from "next/link";
-import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <div className="flex flex-row ml-2 mt-10 gap-4">
         <Link
           href="/chat/buyer"
