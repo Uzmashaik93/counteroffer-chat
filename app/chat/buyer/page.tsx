8fa4e76c58169f702d5642d85b200da35b9f7086
uzma shaik <uzma97910@gmail.com>
2025-07-01 17:57:30 +0530

    feat: create SellerChat component with ChatUI integration

---
+++ app/chat/buyer/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import ChatUI from "@/app/components/ChatUI";
+
+export default function SellerChat() {
+  return (
+    <div>
+      <ChatUI sender="buyer" />
+    </div>
+  );
+}
