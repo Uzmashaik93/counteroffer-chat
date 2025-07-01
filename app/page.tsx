cb52c2bce37a3556e5466dde2f05214d448be9e5
uzma shaik <uzma97910@gmail.com>
2025-07-01 17:58:22 +0530

    feat: update Home component layout with Navbar and chat links

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,29 @@
+import Link from "next/link";
+import Navbar from "./components/Navbar";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Hello
+    <div>
+      <Navbar />
+      <div className="flex flex-row ml-2 mt-10 gap-4">
+        <Link
+          href="/chat/buyer"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:bg-gray-800 bg-black text-white px-2 py-2"
+        >
+          Buyer Chat
+        </Link>
+
+        <Link
+          href="/chat/seller"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:bg-gray-800 bg-black text-white px-2 py-2"
+        >
+          Seller Chat
+        </Link>
+      </div>
     </div>
   );
 }
