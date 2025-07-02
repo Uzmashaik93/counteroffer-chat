fec8dcc717ad74fe3928629ef88a3cd12abba57d
uzma shaik <uzma97910@gmail.com>
2025-07-02 17:04:17 +0530

    fix: update socket connection

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,5 +1,8 @@
 import { io } from 'socket.io-client';
 
-const socket = io(process.env.NEXT_PUBLIC_SOCKET_SERVER_URL!);
+
+const socket = io(process.env.NEXT_PUBLIC_SOCKET_SERVER_URL!, {
+    transports: ["websocket"],
+});
 
 export default socket;
